8c2a3a085898ba6411009671a7bc0c8d509150f5
ChibiAngrboda <122790428+ChibiAngrboda@users.noreply.github.com>
2024-09-24 15:29:11 +0200

    input + système de caméra qui suit le joueur si au mileu t'as vu

--- Space odyssey/Assets/Scripts/player.cs
+++ Space odyssey/Assets/Scripts/player.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    
+    public GameObject cam;
+    Rigidbody2D m_rigidbody;
+    public float Pousée = 28f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.Find("Main Camera");
+
+        m_rigidbody = GetComponent <Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // caméra qui suit un peu mais pas trop
+        if (gameObject.transform.position.y >= 0 && gameObject.transform.position.y <= 10)
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x, gameObject.transform.position.y, cam.transform.position.z);
+        }
+        
+
+        //controles : 
         if (Input.GetMouseButton(0))
         {
             print("rien que pour toi bébou léandre");
+
+            m_rigidbody.AddForce(transform.up, (ForceMode2D)Pousée);
         }
+
+
         /*if (Input.GetMouseButton(1))
         {
             print("yeahhhh");
