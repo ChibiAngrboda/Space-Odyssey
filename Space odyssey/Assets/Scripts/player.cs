415a5c959b09cb52ad2ebc6d5a762834e944d3e1
ChibiAngrboda <122790428+ChibiAngrboda@users.noreply.github.com>
2024-09-24 15:01:05 +0200

    tiens ta scène mattéo

---
+++ Space odyssey/Assets/Scripts/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("rien que pour toi bébou léandre");
+        }
+        /*if (Input.GetMouseButton(1))
+        {
+            print("yeahhhh");
+        }*/
+    }
+}
