4142576fff9409f604870cf61b20e73447181dec
ChibiAngrboda <122790428+ChibiAngrboda@users.noreply.github.com>
2024-09-25 11:21:09 +0200

    ajout des fioles et des obstacles, ajout du mouvement sur x

--- Space odyssey/Assets/Scripts/player.cs
+++ Space odyssey/Assets/Scripts/player.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    
-    public GameObject cam;
     Rigidbody2D m_rigidbody;
+    [Header("Caméra")]
+    public GameObject cam;
+    public float camShift;
+    [Space(5)]
+    [Header("Mouvements")]
     public float thrust = 10f;
     public float stopThrust;
+    public float speed;
+    public float slow;
+    public float acceleration;
+    [Space(5)]
+    [Header("Collectables")]
+    public int fioleNB;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +29,9 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // caméra qui suit un peu mais pas trop
+        // caméra qui suit sur x 
+        cam.transform.position = new Vector3(gameObject.transform.position.x + camShift, cam.transform.position.y, cam.transform.position.z);
+        // caméra qui suit sur y un peu mais pas trop
         if (gameObject.transform.position.y >= 0 && gameObject.transform.position.y <= 10)
         {
             cam.transform.position = new Vector3(cam.transform.position.x, gameObject.transform.position.y, cam.transform.position.z);
@@ -42,7 +53,36 @@ public class player : MonoBehaviour
             m_rigidbody.velocity = Vector3.up * -stopThrust;
         }
 
-     
+        // avancée du joueur
+
+        m_rigidbody.AddForce(transform.right, (ForceMode2D)(speed * Time.fixedDeltaTime));
+       
+
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+        gameObject.transform.position += new Vector3(speed, 0, 0);
+        speed += acceleration;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Fiole")
+        {
+            Destroy(collision.gameObject);
+            fioleNB += 1;
+            
+        }
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Destroy(collision.gameObject);
+            speed = speed * slow;
+        }
 
     }
 }
