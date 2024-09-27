65b879c93f17d8b19e9df56aff55d1e364d4a1f3
ChibiAngrboda <122790428+ChibiAngrboda@users.noreply.github.com>
2024-09-27 14:02:59 +0200

    qd

--- Space odyssey/Assets/Scripts/Volcano.cs
+++ Space odyssey/Assets/Scripts/Volcano.cs
@@ -7,17 +7,21 @@ public class Volcano : MonoBehaviour
     public GameObject rock;
     public float cooldown;
     public float baseCD;
+    public GameObject player;
+    public float playerDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        playerDistance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+        
         cooldown -= 1;
-        if (cooldown <= 0)
+        if (cooldown <= 0 && playerDistance>3)
         {
             Instantiate(rock,new Vector3(gameObject.transform.position.x,gameObject.transform.position.y + 2.5f, 0), new Quaternion(0,0,0,0));
             cooldown += baseCD;
