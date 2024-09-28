b68843bcfcbd6755f939fb5de2fc115452a212ea
ChibiAngrboda <122790428+ChibiAngrboda@users.noreply.github.com>
2024-09-28 11:52:27 +0200

    music

--- Space odyssey/Assets/Scripts/Menu.cs
+++ Space odyssey/Assets/Scripts/Menu.cs
@@ -10,23 +10,20 @@ public class Menu : MonoBehaviour
     //public TMPro bestScoreTxt; 
     public TMP_Text bestScoreText;
     public TMP_Text lastScoreText;
+    public Button Play;
+    public AudioSource playSfx;
     private void Start()
     {
         int bestScore = PlayerPrefs.GetInt("BestScore", 0);
         bestScoreText.text = "Best Score"+ "\n" + bestScore;
         int lastScore = PlayerPrefs.GetInt("lastScore", 0);
         lastScoreText.text = "Last Score"+ "\n" + lastScore;
+        Play.onClick.AddListener(PlaySound);
     }
 
-    public void ResetScore()
+   void PlaySound()
     {
-        PlayerPrefs.SetInt("BestScore", 0);
-        PlayerPrefs.SetInt("lastScore", 0);
-
-        int bestScore = PlayerPrefs.GetInt("BestScore", 0);
-        bestScoreText.text = "Best Score" + "\n" + bestScore;
-        int lastScore = PlayerPrefs.GetInt("lastScore", 0);
-        lastScoreText.text = "Last Score" + "\n" + lastScore;
+        playSfx.Play();
     }
     public void StartGame()
     {
